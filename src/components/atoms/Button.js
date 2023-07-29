c9651e57afaeff461892f6568807ce2341f3812f
Paula Velásquez <pvelaskez@gmail.com>
2023-07-29 17:49:18 -0400

    modificación Button

--- src/components/atoms/Button.js
+++ src/components/atoms/Button.js
@@ -1,9 +1,34 @@
 import React from 'react'
 
-const button = () => {
+const Button = () => {
   return (
-    <div>button</div>
+    <>
+    <button>Agregar</button>
+    <style jsx>{`
+    button {
+        color: rgba(54, 120, 61);
+        background-color:  rgba(173, 227, 115);
+        padding: 10px 50px;
+        border-radius: 5px;
+        font-size: 1rem;
+        border: 0;
+        transition: 0.3s;
+        margin-top: 10px;
+        margin-left: 20px;
+        font-weight: bold;
+        
+    }
+
+
+    button:hover {
+        background-color:  rgba(173, 227, 115, 0.6);
+    }
+
+    
+    `}</style>
+    </>
+    
   )
 }
 
-export default button
+export default Button
