7ee9d313d1c4e338137ccbdb1ce04dd8c67b2c3a
Paula Velásquez <pvelaskez@gmail.com>
2023-07-29 17:52:31 -0400

    modificación Card

--- src/components/atoms/Card.js
+++ src/components/atoms/Card.js
@@ -1,9 +1,64 @@
-import React from 'react'
 
-const Card = () => {
+import styled from 'styled-components'
+import React from 'react'
+import Button from './button'
+
+const Card = (props) => {
   return (
-    <div>Card</div>
+    
+    <Cards>
+        <Image src= {props.data.img} alt=""/>
+        <figcaption>
+            <Producto>{props.data.title}</Producto>
+        </figcaption>
+                                                
+            <Precio >{props.data.precio}</Precio><Peso>1 kg.</Peso>
+        <Button />
+    </Cards>
   )
 }
 
 export default Card
+
+const Cards = styled.figure`
+width: 200px;
+height: 310px;
+border: 2px solid white;
+border-radius: 5px;
+background-color: white;
+margin-left: 0;
+margin-right: 0;
+
+
+`
+//box-shadow: 2px 2px 10px
+
+const Image = styled.img`
+width: 100%;
+height: 50%;
+object-fit:cover;
+`
+
+const Producto = styled.h3`
+line-height: 2;
+margin-left: 1.5rem;
+font-size: 1rem;
+`
+const Precio = styled.a
+`
+font-size: 1.2rem;
+margin-right: 3.5rem;
+margin-left: 1.7rem;
+color: rgba(97, 149, 102);
+border-radius: 5px;git
+
+`
+const Peso = styled.a
+`
+font-size: 0.9rem;
+color: rgba(165, 166, 168);
+background-color: rgb(233, 233, 233);
+padding: 5px 19px;
+border-radius: 5px;
+
+`
